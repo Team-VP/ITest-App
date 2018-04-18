205d9b25caa2c3a42420ff23dae2d7a9cb90ddd2
plmntnsv <plamen.atanasov91@gmail.com>
2018-04-18 23:28:30 +0300

    Added mapping settings

--- ITestApp/ITestApp.Web/Properties/MappingSettings.cs
+++ ITestApp/ITestApp.Web/Properties/MappingSettings.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using ITestApp.Data.Models;
+using ITestApp.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +19,11 @@ namespace ITestApp.Web.Properties
             //       .ForMember(x => x.Author, options => options.MapFrom(x => x.Author.Email));
 
             //this.CreateMap<PostViewModel, PostDto>(MemberList.Source);
-            //this.CreateMap<PostDto, Post>(MemberList.Source);
-            //this.CreateMap<CommentDto, Comment>(MemberList.Source);
+            this.CreateMap<AnswerDto, Answer>(MemberList.Source);
+            this.CreateMap<CategoryDto, Category>(MemberList.Source);
+            this.CreateMap<QuestionDto, Question>(MemberList.Source);
+            this.CreateMap<StatusDto, Status>(MemberList.Source);
+            this.CreateMap<TestDto, Test>(MemberList.Source);
         }
     }
 }
