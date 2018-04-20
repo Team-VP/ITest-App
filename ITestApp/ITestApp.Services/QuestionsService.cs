c1354b9fbe0991864967ee6db06dd77ae5d438ff
borschetsky <wod.moshkin@gmail.com>
2018-04-20 20:52:46 +0300

    Question service mthods were implemented

--- ITestApp/ITestApp.Services/QuestionsService.cs
+++ ITestApp/ITestApp.Services/QuestionsService.cs
@@ -7,6 +7,7 @@ using ITestApp.Data.Repository;
 using ITestApp.Data.Saver;
 using ITestApp.DTO;
 using ITestApp.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITestApp.Services
 {
@@ -18,30 +19,56 @@ namespace ITestApp.Services
 
         public QuestionsService(ISaver saver, IMappingProvider mapper, IRepository<Question> questions)
         {
-            this.saver = saver;
-            this.mapper = mapper;
-            this.questions = questions;
+            this.saver = saver ?? throw new ArgumentNullException("Saver can not be null.");
+            this.mapper = mapper ?? throw new ArgumentNullException("Mapper can not be null.");
+            this.questions = questions ?? throw new ArgumentNullException("Qestion repo can not be null.");
         }
 
         public void Edit(QuestionDto question)
         {
-            throw new System.NotImplementedException();
+            Question questionToEdit = questions.All
+                .Where(q => q.Id == question.Id).Include(a => a.Answers)
+                .FirstOrDefault() ?? throw new ArgumentNullException("Question can not be null.");
+
+            questionToEdit.Text = question.Text;
+            questionToEdit.TestId = question.TestId;
+            
+
+            questions.Update(questionToEdit);
+            saver.SaveChanges();
         }
 
         public IEnumerable<AnswerDto> GetAnswers(int questionId)
         {
-            throw new System.NotImplementedException();
+            var answers = questions.All
+                .Where(q => q.Id == questionId)
+                .Include(a => a.Answers)
+                ?? throw new ArgumentNullException("Collection of answers can not be Null.");
+
+            IQueryable<AnswerDto> result = mapper.ProjectTo<AnswerDto>(answers)
+                ?? throw new ArgumentNullException("Collection of AnswerDTOs can not be null.");
+
+            return result;
         }
 
-        public QuestionDto GetById(int id)
+        public QuestionDto GetQuestionById(int id)
         {
-            throw new System.NotImplementedException();
+            Question question = questions.All
+                .Where(q => q.Id == id)
+                .Include(a => a.Answers).FirstOrDefault() 
+                ?? throw new ArgumentNullException("Question can not be null.");
+
+            QuestionDto result = mapper.MapTo<QuestionDto>(question) 
+                ?? throw new ArgumentNullException("QuestionDTO can not be null.");
+
+            return result;
         }
 
         public void DeleteQuestion(int id)
         {
-            var questionToDelete = questions.All
-                .FirstOrDefault(q => q.Id == id) ?? throw new ArgumentNullException("Question can not be null.");
+            Question questionToDelete = questions.All
+                .FirstOrDefault(q => q.Id == id) 
+                ?? throw new ArgumentNullException("Question can not be null.");
 
             questions.Delete(questionToDelete);
         }
