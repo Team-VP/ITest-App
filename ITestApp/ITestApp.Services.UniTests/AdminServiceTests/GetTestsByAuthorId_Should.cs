3ad06e211b1eed693f091276f7be15278d303032
Viacheslav <wod.moshkin@gmail.com>
2018-05-05 20:14:49 +0300

    InvokeMapper Tests added to AdminService

--- ITestApp/ITestApp.Services.UniTests/AdminServiceTests/GetTestsByAuthorId_Should.cs
+++ ITestApp/ITestApp.Services.UniTests/AdminServiceTests/GetTestsByAuthorId_Should.cs
@@ -86,5 +86,44 @@ namespace ITestApp.Services.UniTests.AdminServiceTests
 
             Assert.ThrowsException<ArgumentException>(() => adminService.GetTestsByAuthor(String.Empty));
         }
+
+        [TestMethod]
+        public void Invoke_Mapper_ProjectTo()
+        {
+            //Arrange
+            var mapperMock = new Mock<IMappingProvider>();
+            var saverMock = new Mock<ISaver>();
+            var testsRepoMock = new Mock<IRepository<Test>>();
+            var userTestsRepoMock = new Mock<IRepository<UserTest>>();
+
+            var author = new User() { UserName = "a@a.com", Id = "1" };
+            var data = new List<Test>()
+            {
+                new Test { Title = "Test1", Author = author, AuthorId = "1"},
+                new Test { Title = "Test2", Author = author, AuthorId = "1"},
+                new Test { Title = "Test3", Author = author, AuthorId = "1"}
+            }.AsQueryable();
+
+            testsRepoMock.Setup(x => x.All).Returns(data);
+
+            var dtoAuthor = new UserDto() { UserName = "a@a.com", Id = "1" };
+            var mapperData = new List<TestDto>()
+            {
+                new TestDto { Title = "Test1", Author = dtoAuthor, AuthorId = "1"},
+                new TestDto { Title = "Test2", Author = dtoAuthor, AuthorId = "1"},
+                new TestDto { Title = "Test3", Author = dtoAuthor, AuthorId = "1"}
+            };
+
+            mapperMock.Setup(m => m.ProjectTo<TestDto>(It.IsAny<IQueryable<Test>>())).Returns(new List<TestDto>(mapperData).AsQueryable);
+            var adminService = new AdminService(saverMock.Object, mapperMock.Object, testsRepoMock.Object, userTestsRepoMock.Object);
+            data.ToList();
+
+            //Act
+            var tests = adminService.GetTestsByAuthor(author.Id);
+            
+            //Assert
+            mapperMock.Verify(x => x.ProjectTo<TestDto>(It.IsAny<IQueryable<Test>>()), Times.Once);
+
+        }
     }
 }
---
+++ ITestApp/ITestApp.Services.UniTests/AdminServiceTests/GetUserResults_Should.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using AutoMapper;
+using ITestApp.Common.Providers;
+using ITestApp.Data;
+using ITestApp.Data.Models;
+using ITestApp.Data.Repository;
+using ITestApp.Data.Saver;
+using ITestApp.DTO;
+using ITestApp.Services.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+
+namespace ITestApp.Services.UniTests.AdminServiceTests
+{
+    [TestClass]
+    public class GetUserResults_Should
+    {
+        [TestMethod]
+        public void ReturnIEnumerableOfUserTestDto_WhenCollectionIsNotNull()
+        {
+            //Arrange
+            var mapperMock = new Mock<IMappingProvider>();
+            var saverMock = new Mock<ISaver>();
+            var testsRepoMock = new Mock<IRepository<Test>>();
+            var userTestsRepoMock = new Mock<IRepository<UserTest>>();
+
+            var data = new List<UserTest>()
+            {
+                new UserTest{UserId = "1", Id = 1, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTest{UserId = "1", Id = 2, TestId = 2, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTest{UserId = "2", Id = 3, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+            }.AsQueryable();
+
+            userTestsRepoMock.Setup(x => x.All).Returns(data);
+
+            var dataMapper = new List<UserTestDto>()
+            {
+                new UserTestDto{UserId = "1", Id = 1, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTestDto{UserId = "1", Id = 2, TestId = 2, IsPassed = true , TimeExpire = DateTime.Now },
+                new UserTestDto{UserId = "2", Id = 3, TestId = 1, IsPassed = true , TimeExpire = DateTime.Now },
+            };
+
+            mapperMock.Setup(m => m.ProjectTo<UserTestDto>(It.IsAny<IQueryable<UserTest>>())).Returns(new List<UserTestDto>(dataMapper).AsQueryable());
+
+            //Act
+            var adminService = new AdminService(saverMock.Object, mapperMock.Object, testsRepoMock.Object, userTestsRepoMock.Object);
+            data.ToList();
+
+            var usersTests = adminService.GetUserResults();
+            //Assert
+            Assert.AreEqual(data.Count(), usersTests.Count());
+            Assert.IsNotNull(usersTests);
+            Assert.IsInstanceOfType(usersTests, typeof(IEnumerable<UserTestDto>));
+        }
+
+        [TestMethod]
+        public void Invoke_Mapper_ProjectTo()
+        {
+            //Arrange
+            var mapperMock = new Mock<IMappingProvider>();
+            var saverMock = new Mock<ISaver>();
+            var testsRepoMock = new Mock<IRepository<Test>>();
+            var userTestsRepoMock = new Mock<IRepository<UserTest>>();
+
+            var data = new List<UserTest>()
+            {
+                new UserTest{UserId = "1", Id = 1, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTest{UserId = "1", Id = 2, TestId = 2, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTest{UserId = "2", Id = 3, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+            }.AsQueryable();
+
+            userTestsRepoMock.Setup(x => x.All).Returns(data);
+
+            var dataMapper = new List<UserTestDto>()
+            {
+                new UserTestDto{UserId = "1", Id = 1, TestId = 1, IsPassed = true, TimeExpire = DateTime.Now  },
+                new UserTestDto{UserId = "1", Id = 2, TestId = 2, IsPassed = true , TimeExpire = DateTime.Now },
+                new UserTestDto{UserId = "2", Id = 3, TestId = 1, IsPassed = true , TimeExpire = DateTime.Now },
+            };
+
+            mapperMock.Setup(m => m.ProjectTo<UserTestDto>(It.IsAny<IQueryable<UserTest>>())).Returns(new List<UserTestDto>(dataMapper).AsQueryable());
+
+            //Act
+            var adminService = new AdminService(saverMock.Object, mapperMock.Object, testsRepoMock.Object, userTestsRepoMock.Object);
+            data.ToList();
+            var usersTests = adminService.GetUserResults();
+            //Assert
+            mapperMock.Verify(x => x.ProjectTo<UserTestDto>(It.IsAny<IQueryable<UserTest>>()), Times.Once);
+        }
+    }
+}
