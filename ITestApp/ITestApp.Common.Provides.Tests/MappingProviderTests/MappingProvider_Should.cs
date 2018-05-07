a7ba4949a5ca66246c54dde8d96952c9e47d6e98
Viacheslav <wod.moshkin@gmail.com>
2018-05-07 12:35:00 +0300

    MappingProvider Tests Implemented

---
+++ ITestApp/ITestApp.Common.Provides.Tests/MappingProviderTests/MappingProvider_Should.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ITestApp.Common.Providers;
+using ITestApp.Data.Models;
+using ITestApp.DTO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ITestApp.Common.Provides.UniTests.MappingProviderTests
+{
+    [TestClass]
+    public class MappingProvider_Should
+    {
+        [TestMethod]
+        public void CallMethodMap()
+        {
+            //Arrange
+            var test = new Test();
+
+            var mapperMock = new Mock<IMapper>();
+
+            mapperMock.Setup(m => m.Map<TestDto>(It.IsAny<Test>())).Verifiable();
+
+            var mapperProvider = new MappingProvider(mapperMock.Object);
+
+            //Act
+            var dto = mapperProvider.MapTo<TestDto>(test);
+
+            //Assert
+            mapperMock.Verify(x => x.Map<TestDto>(test), Times.Once);
+        }
+    }
+}
