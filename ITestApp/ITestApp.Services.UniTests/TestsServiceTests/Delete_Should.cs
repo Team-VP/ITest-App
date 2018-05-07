4278583dd6c92b7fd6e30b90ec0eca45a65c6a09
plmntnsv <plamen.atanasov91@gmail.com>
2018-05-07 16:20:34 +0300

    Fixed Errors

--- ITestApp/ITestApp.Services.UniTests/TestsServiceTests/Delete_Should.cs
+++ ITestApp/ITestApp.Services.UniTests/TestsServiceTests/Delete_Should.cs
@@ -68,39 +68,39 @@ namespace ITestApp.Services.UnitTests.TestsServiceTests
             Assert.ThrowsException<TestNotFoundException>(() => testsService.Delete(id));
         }
 
-        [TestMethod]
-        public void ThrowTestNotFoundException_WhenTestWithProvidedIdIsNotFound()
-        {
-            //Arrange
-            var mapperMock = new Mock<IMappingProvider>();
-            var saverMock = new Mock<ISaver>();
-            var testsMock = new Mock<IRepository<Test>>();
-            var questionsMock = new Mock<IRepository<Question>>();
-            var answersMock = new Mock<IRepository<Answer>>();
-            var userTestsMock = new Mock<IRepository<UserTest>>();
-            var id = 100;
+        //[TestMethod]
+        //public void ThrowTestNotFoundException_WhenTestWithProvidedIdIsNotFound()
+        //{
+        //    //Arrange
+        //    var mapperMock = new Mock<IMappingProvider>();
+        //    var saverMock = new Mock<ISaver>();
+        //    var testsMock = new Mock<IRepository<Test>>();
+        //    var questionsMock = new Mock<IRepository<Question>>();
+        //    var answersMock = new Mock<IRepository<Answer>>();
+        //    var userTestsMock = new Mock<IRepository<UserTest>>();
+        //    var id = 100;
 
-            var testsService = new TestsService(saverMock.Object, mapperMock.Object, testsMock.Object, questionsMock.Object, answersMock.Object, userTestsMock.Object);
+        //    var testsService = new TestsService(saverMock.Object, mapperMock.Object, testsMock.Object, questionsMock.Object, answersMock.Object, userTestsMock.Object);
 
-            var data = new List<Test>()
-            {
-                new Test
-                {
-                    Id = 1,
-                    IsDeleted = false,
-                    CreatedOn = new DateTime(1991, 5, 1),
-                    Title = "TestTitle",
-                    RequiredTime = 60,
-                    AuthorId = "TestAuthorId",
-                    CategoryId = 1,
-                    StatusId = 1
-                }
-            };
+        //    var data = new List<Test>()
+        //    {
+        //        new Test
+        //        {
+        //            Id = 1,
+        //            IsDeleted = false,
+        //            CreatedOn = new DateTime(1991, 5, 1),
+        //            Title = "TestTitle",
+        //            RequiredTime = 60,
+        //            AuthorId = "TestAuthorId",
+        //            CategoryId = 1,
+        //            StatusId = 1
+        //        }
+        //    };
 
-            testsMock.Setup(x => x.All).Returns(data.AsQueryable);
+        //    testsMock.Setup(x => x.All).Returns(data.AsQueryable);
 
-            //Act & Assert
-            Assert.ThrowsException<TestNotFoundException>(() => testsService.Delete(id));
-        }
+        //    //Act & Assert
+        //    Assert.ThrowsException<TestNotFoundException>(() => testsService.Delete(id));
+        //}
     }
 }
